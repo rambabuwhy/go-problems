e69f21df9933fa9d34ced6ade5388a2c5c0d63c6
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-12-02 21:28:03 +0530

    var expr

--- Var_Expr/main.go
+++ Var_Expr/main.go
@@ -9,4 +9,23 @@ func main() {
 	y := x + 100
 	fmt.Println(y)
 
+	fmt.Printf("%T  \n", y)
+
+	//string
+	var s string
+	s = "ram"
+	fmt.Println("string s:", s, "adding")
+
+	//integer
+	var i int
+	var j int = 100
+	fmt.Println("integer i:", i, "adding")
+	fmt.Println("integer j:", j, "adding")
+
+	foo()
+
+}
+
+func foo() {
+	fmt.Println("foo  func")
 }
