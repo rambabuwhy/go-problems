7e7edba726fbd5beca35fe49b5add2c066c09397
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-12-03 20:29:56 +0530

    update

--- Var_Expr/main.go
+++ Var_Expr/main.go
@@ -22,7 +22,47 @@ func main() {
 	fmt.Println("integer i:", i, "adding")
 	fmt.Println("integer j:", j, "adding")
 
+	//fmt
+
+	var a int = 10
+	fmt.Printf("%v  \n", a)
+
+	//own type
+
+	type T int
+	var b T = 400
+	fmt.Printf("%T \n", b)
+
+	//convert it to int
+
+	a = int(b)
+	fmt.Printf("%v  \n", a)
+
+	//bool
+
+	var c bool
+	c = true
+	fmt.Println(c)
+
 	foo()
+
+	//types
+
+	var p uint8
+	p = 23
+	fmt.Println(p)
+
+	//bytes
+
+	var str string = " Rambabu Yerajana"
+
+	var B []byte = []byte(str)
+
+	fmt.Println(B)
+
+	for i2 := 0; i2 < len(str); i2++ {
+		fmt.Printf("%#U \n", str[i2])
+	}
 
 }
 
