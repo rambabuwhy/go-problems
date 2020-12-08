075e533c891d68a056e9a691c95de3bea533ebec
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-12-08 17:41:08 +0530

    coursera

--- Coursera-GO/findian.go
+++ Coursera-GO/findian.go
@@ -4,27 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	s, _ := reader.ReadString('\n')
+	s = strings.TrimSpace(s)
 
-	fmt.Println(len(text))
+	if (s[0] == 'i' || s[0] == 'I') && (s[len(s)-1] == 'n' || s[len(s)-1] == 'N') && (strings.Contains(s, "a") || strings.Contains(s, "A")) {
+		fmt.Println("Found!")
+	} else {
 
-	/*
-		if (s[0] == 'i' || s[0] == 'I') &&
-			(s[len(s)-1] == 'n' || s[len(s)-1] == 'N') &&
-			(strings.Contains(s, "a") || strings.Contains(s, "A")) {
-			fmt.Println("Found!")
-		} else {
+		fmt.Println("Not Found!")
+	}
 
-			fmt.Println("Not Found!")
-		}
-
-	*/
 
 }
