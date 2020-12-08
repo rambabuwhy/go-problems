ce4a240f0640587eeae45a1b263301f0bb6c1235
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-12-08 17:25:51 +0530

    coursera

---
+++ Coursera-GO/findian.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+
+	/*
+		if (s[0] == 'i' || s[0] == 'I') &&
+			(s[len(s)-1] == 'n' || s[len(s)-1] == 'N') &&
+			(strings.Contains(s, "a") || strings.Contains(s, "A")) {
+			fmt.Println("Found!")
+		} else {
+
+			fmt.Println("Not Found!")
+		}
+
+	*/
+
+}
---
+++ Coursera-GO/trunc.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var f float32
+	fmt.Scan(&f)
+
+	fmt.Printf("%.f  \n", f)
+}
